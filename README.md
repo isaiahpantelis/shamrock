a9d412a00bdf87b365a7d44eea862896e8850f90
Pantelis Isaiah <pantelis@Panteliss-iMac.local>
2020-01-04 15:09:05 -0500

    Changes

--- README.md
+++ README.md
@@ -12,8 +12,6 @@ def f(x):
 ```
 It describes an exponentially decaying cosine and its graph looks like this
 
-<img src="https://github.com/isaiahpantelis/shamrock/blob/master/img/mexhat_y_plot.png" width="50%">
-
 ![](img/mexhat_y_plot.png)
 
 Suppose, further, that one of the tasks in hand is to find the roots, minimima, and maxima of `f`. Using the `shamrock` python package, this task can be accomplised as follows:
@@ -42,3 +40,32 @@ roots = f.solve()
 Here are the roots superimposed on the graph of `f`:
 
 ![](img/mexhat_y_and_roots_plot.png)
+
+4. Find the local minima and maxima of `f`:
+```python
+critical, argmin, argmax = f.optimise()
+argmin = critical[np.where(argmin == 1)]
+argmax = critical[np.where(argmax == 1)]
+minvals = f.eval(argmin, mode=b'exact')
+maxvals = f.eval(argmax, mode=b'exact')
+```
+
+Plotting the graph of the function `f`, the roots, and the local optima in the same axes
+```python
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(x, y, label='y')
+plt.plot(roots, np.zeros((len(roots),)), marker='.', linewidth=0.0, label='roots')
+plt.plot(argmin, minvals, marker='.', linewidth=0.0, label='minima')
+plt.plot(argmax, maxvals, marker='.', linewidth=0.0, label='maxima')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid()
+plt.legend()
+plt.show()
+```
+
+results in the following plot:
+
+![](img/mexhat_y_and_roots_and_local_optima_plot.png)
--- scripts/examples/mexhat.py
+++ scripts/examples/mexhat.py
@@ -42,75 +42,22 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.grid()
 plt.legend()
+
+# -- OPTIMA
+critical, argmin, argmax = f.optimise()
+argmin = critical[np.where(argmin == 1)]
+argmax = critical[np.where(argmax == 1)]
+minvals = f.eval(argmin, mode=b'exact')
+maxvals = f.eval(argmax, mode=b'exact')
+
+plt.figure()
+plt.plot(x, y, label='y')
+plt.plot(roots, np.zeros((len(roots),)), marker='.', linewidth=0.0, label='roots')
+plt.plot(argmin, minvals, marker='.', linewidth=0.0, label='minima')
+plt.plot(argmax, maxvals, marker='.', linewidth=0.0, label='maxima')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid()
+plt.legend()
+
 plt.show()
-
-# # # -- DIFFERENTIATION
-# # dy1 = f.eval(x, diff_order=1)
-# # # # pprint(f.dcoeffs)
-# # dy2 = f.eval(x, diff_order=2)
-# # # # pprint(f.dcoeffs)
-# # dy3 = f.eval(x, diff_order=3)
-# # dy4 = f.eval(x, diff_order=4)
-# # # # pprint(f.dcoeffs)
-
-# # -- OPTIMA
-# critical, minima, maxima = f.optimise()
-#
-# # print(f'-- critical = {critical}')
-# # print(f'-- minima = {minima}')
-# # print(f'-- maxima = {maxima}')
-#
-# # print(f'[python] f.dcoeffs = {f.dcoeffs}')
-# # print(f'[python] f.dcoeffs = {f.dcoeffs}')
-# # print(f'[python] f.coeffs = {f.coeffs}')
-# # print(f'[python] f.dcoeffs = {f.dcoeffs}')
-# # print(f'[python] order = {len(f.dcoeffs)}')
-# # print(f'[python] #f.dcoeffs = {len(f.dcoeffs[0])}')
-#
-# # -- PLOTS
-# plt.figure()
-# plt.plot(x, y, label='y')
-# # plt.plot(x, yhat, label=f'yhat')
-# # plt.plot(x, dy1, linestyle='--', linewidth=1.0, label=f'dy1')
-# # plt.plot(x, dy2, linestyle='--', linewidth=1.0, label=f'dy2')
-# # plt.plot(x, dy3, linestyle='--', linewidth=1.0, label=f'dy3')
-# # plt.plot(x, dy4, linestyle='--', linewidth=1.0, label=f'dy4')
-# # plt.plot(f.p.x, f.fvals, marker='.', linewidth=0.0, label='fvals')
-# plt.plot(roots, np.zeros((len(roots),)), marker='.', linewidth=0.0, markersize=12, label='roots')
-# # plt.plot(droots, np.zeros((len(droots),)), marker='.', linewidth=0.0, markersize=12, label='droots')
-# # plt.plot(ddroots, np.zeros((len(ddroots),)), marker='.', linewidth=0.0, markersize=12, label='ddroots')
-#
-# # plt.plot(critical, np.zeros((len(critical),)), marker='.', linewidth=0.0, markersize=12, label='critical')
-# minima = np.array([_[0] for _ in zip(critical, minima) if _[1] > 0])
-# plt.plot(minima, f(minima), marker='.', linewidth=0.0, markersize=12, label='minima')
-# maxima = np.array([_[0] for _ in zip(critical, maxima) if _[1] > 0])
-# plt.plot(maxima, f(maxima), marker='.', linewidth=0.0, markersize=12, label='maxima')
-#
-# plt.legend()
-# plt.grid()
-# # plt.show()
-#
-# # -- ERROR
-# # plt.figure()
-# # plt.plot(x, y - yhat, label='error')
-# # plt.legend()
-# # plt.grid()
-#
-# # -- COEFFS
-# coeffs = f.coeffs[0]
-# plt.figure()
-# plt.plot(list(range(len(coeffs))), coeffs, linestyle='--', marker='.', label='coeffs')
-# plt.legend()
-# plt.grid()
-# # plt.show()
-#
-# # # -- Root checking
-# # vals = []
-# # for root in roots:
-# #     vals.append(f(np.array(root, ndmin=1)))
-# # plt.figure()
-# # plt.plot(roots, vals, linestyle='--', linewidth=0, marker='.', label='values at roots')
-# # plt.legend()
-# # plt.grid()
-#
-# plt.show()
